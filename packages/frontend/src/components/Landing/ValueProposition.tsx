6c8b222f41aa59cab311d92d682d55b350dfb045
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2025-05-07 17:23:09 -0700

    removed unused nested divs

--- packages/frontend/src/components/Landing/ValueProposition.tsx
+++ packages/frontend/src/components/Landing/ValueProposition.tsx
@@ -3,20 +3,16 @@
 import ResumeTailoringSection from '../ResumeTailoringSection'
 import ApplicationAutofill from './ApplicationAutofill'
 import ApplicationTracking from './ApplicationTracking'
-import ResumeTailoring from './ResumeTailoring'
 
 const ValueProposition = () => {
   return (
-    <section id="features" className="section bg-gray-50 py-16 md:py-24">
-      <div className="container mx-auto px-4">
-        <div className="max-w-5xl mx-auto">
-          <ResumeTailoring />
-          <ResumeTailoringSection />
-          <ApplicationAutofill />
-          <ApplicationTracking />
-        </div>
+    <div className="mx-auto w-full">
+      <ResumeTailoringSection />
+      <div className="max-w-5xl mx-auto">
+        <ApplicationAutofill />
+        <ApplicationTracking />
       </div>
-    </section>
+    </div>
   )
 }
 
