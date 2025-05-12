fa14b0fa22d00b4146bff66b7e46cfc6ee7aeead
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2025-05-12 16:15:02 -0700

    added initial resume segmentation logic

--- packages/backend/src/backend/resumes.py
+++ packages/backend/src/backend/resumes.py
@@ -31,11 +31,35 @@ def fetch_resume(userId: str, resumeName: str):
 
 def parse_resume(resumePath: str):
     doc = Document(resumePath)
+    segments = segment_resume(doc)
+
+    for section, content in segments.items():
+        print(f"Section: {section}")
+        for paragraph in content:
+            print(paragraph)
+        print("\n")
+
+
+def segment_resume(doc: Document):
+    """
+    Segments the resume into sections based on headings.
+    """
+    sections = {}
+
+    current_section_heading = "intro"
+    prev_para_was_heading = False
 
     for p in iter_doc_paragraphs(doc):
-        if is_likely_heading(p, doc):
-            print(f"Heading: {p.text}")
-            pass
+        if is_likely_heading(p, doc) and not prev_para_was_heading:
+            prev_para_was_heading = True
+            current_section_heading = p.text
+            sections[current_section_heading] = []
+
+        else:
+            sections[current_section_heading].append(p.text)
+            prev_para_was_heading = False
+
+    return sections
 
 
 if __name__ == "__main__":
