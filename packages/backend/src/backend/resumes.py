a45af274dab101ee89b9c5644527d7fb36f6aba0
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2025-05-12 18:45:29 -0700

    cleanup imports

--- packages/backend/src/backend/resumes.py
+++ packages/backend/src/backend/resumes.py
@@ -1,15 +1,7 @@
 import os
 from docx import Document
-from docx.text.paragraph import Paragraph
 from firebase_admin import storage
-
-from docx.oxml.ns import qn
-
 from backend.firebase import init_firebase
-
-import re
-from docx.oxml.ns import qn
-from docx import Document
 
 from backend.docx_functions import is_likely_heading, iter_doc_paragraphs
 from backend.util import clean_heading_text, print_sections
@@ -78,6 +70,6 @@ def segment_resume(doc: Document):
 
 if __name__ == "__main__":
     # resumePath = fetch_resume("testUserId", "V3 Compressed Fabric.docx")
-    # resumePath = "resumes/testUserId/V3Resume.docx"
-    resumePath = "resumes/testUserId/Senior-Product-Manager-Resume-Example.docx"
+    resumePath = "resumes/testUserId/V3Resume.docx"
+    # resumePath = "resumes/testUserId/Senior-Product-Manager-Resume-Example.docx"
     parse_resume(resumePath)
