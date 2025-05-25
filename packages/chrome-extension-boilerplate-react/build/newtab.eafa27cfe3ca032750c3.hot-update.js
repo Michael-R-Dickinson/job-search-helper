7c0cbed2d26496b2ea33e1bd08b3719831ddb61a
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2025-05-25 13:29:13 -0700

    renamed chrome extension boilerplate to extension

---
+++ packages/chrome-extension-boilerplate-react/build/newtab.eafa27cfe3ca032750c3.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdatechrome_extension_boilerplate_react"]("newtab",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("813895f34a9c19d62bf4")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=newtab.eafa27cfe3ca032750c3.hot-update.js.map
