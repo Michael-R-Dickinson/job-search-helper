e3fe7d8c5cfd87a5f4b9453a2903b2b4da776fe2
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2025-05-24 22:19:11 -0700

    init chrome extension through boilerplate

--- packages/chrome-extension/vite.config.ts
+++ packages/chrome-extension/vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { chromeExtension } from "vite-plugin-chrome-extension"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), chromeExtension()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+
+  build: {
+    rollupOptions: {
+      input: {
+        input: "src/manifest.json",
+        popup: "src/popup.html",
+        background: "src/background.ts",
+        contentScript: "src/contentScript.ts",
+      },
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
 })
