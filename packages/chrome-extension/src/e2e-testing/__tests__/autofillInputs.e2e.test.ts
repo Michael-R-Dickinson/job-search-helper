dfce390f4a3d1c8ef5b6cfe2fcd2f450a038b7dc
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2025-05-30 17:59:34 -0700

    added input autofilling tests

---
+++ packages/chrome-extension/src/e2e-testing/__tests__/autofillInputs.e2e.test.ts
@@ -0,0 +1,25 @@
+import { describe, it, expect } from 'vitest'
+import { resurrectInputInfosFromTest } from '../saveInputInfosForTests'
+import { serializeInputsHtml } from '../../content/serializeInputsHtml'
+import saveAutofillValues from '../../autofillEngine/saveAutofillValues'
+import { testWebsites, TEST_USER_ID } from './saveFilledInputs.e2e.test'
+import getAutofillValues from '../../autofillEngine/getAutofillValues'
+
+describe('End-to-end save feature', () => {
+  testWebsites.forEach((websiteTestData: any) => {
+    it(`should correctly save autofill values for ${websiteTestData.sourceURL}`, async () => {
+      const autofillInstructionsExpected = websiteTestData.autofillInstructionsResponse
+      // Ressurect inputInfos from the test data - essentially simulating the inputs from the website
+      const inputInfos = resurrectInputInfosFromTest(websiteTestData.inputsData)
+      // We use the flow for saving filled values - tested in saveFilledInputs.e2e.test.ts - to load the test
+      // values into the database, so when we autofill we gaurantee we're pulling from the right values
+      const serializedInputs = serializeInputsHtml(inputInfos)
+      await saveAutofillValues(serializedInputs, TEST_USER_ID)
+      // Then we take the flow from triggerGetAutofillValues and getAutofillValues to get autofill instructions
+      // And these we validate as they're the final result of autofilling
+      const autofillInstructions = await getAutofillValues(serializedInputs, TEST_USER_ID)
+      // We validate the autofill instructions against the test data
+      expect(autofillInstructions).toEqual(autofillInstructionsExpected)
+    })
+  })
+})
--- packages/chrome-extension/src/e2e-testing/__tests__/saveFilledInputs.e2e.test.ts
+++ packages/chrome-extension/src/e2e-testing/__tests__/saveFilledInputs.e2e.test.ts
@@ -6,23 +6,16 @@ import { serializeInputsHtml } from '../../content/serializeInputsHtml'
 import saveAutofillValues from '../../autofillEngine/saveAutofillValues'
 
 // Load the test cases JSON
-const testCasesPath = path.resolve(__dirname, 'inputsTestCases.json')
-const testSites = JSON.parse(fs.readFileSync(testCasesPath, 'utf-8'))
+export const testCasesPath = path.resolve(__dirname, 'inputsTestCases.json')
+export const testWebsites = JSON.parse(fs.readFileSync(testCasesPath, 'utf-8'))
 
-const TEST_USER_ID = 'test-user-id'
+export const TEST_USER_ID = 'test-user-id'
 describe('End-to-end save feature', () => {
-  testSites.forEach((websiteTestData: any) => {
+  testWebsites.forEach((websiteTestData: any) => {
     it(`should correctly save autofill values for ${websiteTestData.sourceURL}`, async () => {
       // 1. Resurrect InputInfo[] from serialized data
       const inputInfos = resurrectInputInfosFromTest(websiteTestData.inputsData)
       const filledInputsExpected = websiteTestData.saveFilledInputsResponse
-
-      // Debug: print resurrected inputInfos and their values
-      inputInfos.forEach((info, idx) => {
-        console.log(
-          `Resurrected input #${idx}: label=${info.label}, id=${info.element.id}, value=${(info.element as any).value}`,
-        )
-      })
 
       // Take preprocessing from triggerSaveFilledValues.ts
       // But manually pass it to saveAutofillValues so we don't rely on the chrome
@@ -30,16 +23,6 @@ describe('End-to-end save feature', () => {
       const serializedInputs = serializeInputsHtml(inputInfos)
       const saveFilledInputsResponse = await saveAutofillValues(serializedInputs, TEST_USER_ID)
 
-      // console.log('saveFilledInputsResponse', saveFilledInputsResponse)
-      // 3. Validate that the saveFilledInputsResponse matches the expected response
-      console.log(
-        'Expected saveFilledInputsResponse:',
-        JSON.stringify(filledInputsExpected, null, 2),
-      )
-      console.log(
-        'Actual saveFilledInputsResponse:',
-        JSON.stringify(saveFilledInputsResponse, null, 2),
-      )
       expect(saveFilledInputsResponse).toEqual(filledInputsExpected)
     })
   })
