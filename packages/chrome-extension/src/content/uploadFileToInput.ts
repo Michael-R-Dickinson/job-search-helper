19483744edbb33225ba7043e8aaa16ad87527a09
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2025-06-04 19:58:16 -0700

    find nearest input element if fileUpload is a button

--- packages/chrome-extension/src/content/uploadFileToInput.ts
+++ packages/chrome-extension/src/content/uploadFileToInput.ts
@@ -1,4 +1,28 @@
-export const fillResumeUploadInput = async (inputElement: HTMLInputElement) => {
+const findNearestInputElement = (element: HTMLElement): HTMLInputElement | null => {
+  const parent = element.parentElement
+  if (!parent) return null
+
+  if (parent instanceof HTMLInputElement) {
+    return parent
+  }
+  for (const child of parent.children) {
+    if (child instanceof HTMLInputElement) {
+      return child
+    }
+  }
+
+  return findNearestInputElement(parent)
+}
+
+export const fillResumeUploadInput = async (inputElement: HTMLInputElement | HTMLButtonElement) => {
+  let correctedInputElement: HTMLInputElement | null = null
+  if (inputElement instanceof HTMLButtonElement) {
+    correctedInputElement = findNearestInputElement(inputElement)
+  } else {
+    correctedInputElement = inputElement
+  }
+  if (correctedInputElement === null) return
+
   // 1. Build a URL that points to the test‐resume PDF inside assets/
   const fileUrl = chrome.runtime.getURL('src/assets/VasdevYash-Resume.pdf')
 
@@ -14,9 +38,8 @@ export const fillResumeUploadInput = async (inputElement: HTMLInputElement) => {
   dt.items.add(file)
 
   // 5. Assign to the input element
-  inputElement.files = dt.files
-  console.log('inputElement.files', inputElement.files)
+  correctedInputElement.files = dt.files
 
   // 6. Fire a "change" event so that any listener on this input sees it as filled
-  inputElement.dispatchEvent(new Event('change', { bubbles: true }))
+  correctedInputElement.dispatchEvent(new Event('change', { bubbles: true }))
 }
