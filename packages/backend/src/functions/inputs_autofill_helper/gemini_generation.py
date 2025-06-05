f5cfd5b7d941edd64cc76098d2f9c1893a223292
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2025-06-04 20:17:06 -0700

    added back input labels for better model performance

--- packages/backend/src/functions/inputs_autofill_helper/gemini_generation.py
+++ packages/backend/src/functions/inputs_autofill_helper/gemini_generation.py
@@ -84,6 +84,7 @@ class IfExpression(BaseModel):
 class AutofillInstruction(BaseModel):
     input_id: str
     valuePathString: Union[str, IfExpression]
+    initialLabel: str
 
 
 class AutofillResponseSchema(BaseModel):
@@ -96,11 +97,11 @@ def generate_autofill_with_gemini(inputs) -> AutofillResponseSchema:
             system_instruction=SYSTEM_INSTRUCTION,
             response_mime_type="application/json",
             response_schema=AutofillResponseSchema,
-            temperature=0.0,
-            # thinking_config=types.ThinkingConfig(thinking_budget=0),
+            # temperature=0.0,
+            thinking_config=types.ThinkingConfig(thinking_budget=0),
         ),
-        # model="gemini-2.5-flash-preview-04-17",
-        model="gemini-2.0-flash",
+        model="gemini-2.5-flash-preview-04-17",
+        # model="gemini-2.0-flash",
     )
     response = chat.send_message(
         json.dumps(inputs),
