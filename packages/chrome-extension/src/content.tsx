2b5ff61b698a5ae723c85923c7d0267690c30299
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2025-06-06 15:02:24 -0700

    added fonts

--- packages/chrome-extension/src/content.tsx
+++ packages/chrome-extension/src/content.tsx
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Sidebar from './content/Sidebar'
 
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
+
 const root = document.createElement('div')
 root.id = 'crx-root'
 document.body.appendChild(root)
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <Sidebar />
+    <div
+      style={{
+        fontFamily: 'Inter, sans-serif',
+      }}
+    >
+      <Sidebar />
+    </div>
   </React.StrictMode>,
 )
