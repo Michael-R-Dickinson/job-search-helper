f5f5c9ae2a235a5d9ed712bd115a625b54fe27c8
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2025-06-21 15:20:01 -0700

    clean out unused code and return filled inputs from get_filled_inputs

--- packages/backend/src/functions/inputs_autofill_helper/fill_inputs.py
+++ packages/backend/src/functions/inputs_autofill_helper/fill_inputs.py
@@ -14,6 +14,7 @@ def get_filled_inputs(user_id, inputs: InputList):
         raise ValueError("User data not found")
 
     classified_inputs = get_input_classifications(inputs)
+    print("classified\n", classified_inputs.model_dump(), "\n\n")
 
     filled_inputs = []
     for classified_input in classified_inputs:
@@ -32,12 +33,4 @@ def get_filled_inputs(user_id, inputs: InputList):
 
     print("filled_inputs\n", json.dumps(filled_inputs, indent=4))
 
-    # autofill_template_values = generate_autofill_with_gemini(inputs)
-    # autofill_instructions = map_autofill_template_to_instructions(
-    #     autofill_template_values, user_autofill_data, original_inputs=inputs
-    # )
-    # print("\nInitial Inputs: ", inputs, "\n")
-    # print("Autofill Template: ", autofill_template_values.model_dump(), "\n")
-    # print("Finished Autofill Instructions: ", autofill_instructions, "\n\n")
-
-    # return autofill_instructions
+    return filled_inputs
