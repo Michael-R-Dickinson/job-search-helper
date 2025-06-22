a7384c2358dbf3870dd63d345b72821371bb9e07
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2025-06-22 03:16:26 -0700

    updated base category handlers with SimpleTextOnlyCategoryHandler for simple text cases

--- packages/backend/src/functions/inputs_autofill_helper/category_handlers/base_category_handler.py
+++ packages/backend/src/functions/inputs_autofill_helper/category_handlers/base_category_handler.py
@@ -1,5 +1,5 @@
-from abc import ABC
-from typing import Union
+from abc import ABC, abstractmethod
+from typing import Union, override
 
 from functions.inputs_autofill_helper.autofill_schema import (
     ClassifiedInput,
@@ -7,6 +7,9 @@ from functions.inputs_autofill_helper.autofill_schema import (
     FieldType,
     SaveInstruction,
 )
+
+from dictor import dictor
+
 
 DECLINE_ANSWER_CANONICALS = [
     "prefer not to say",
@@ -22,10 +25,6 @@ def get_decline_answer_canonicals():
 class BaseCategoryHandler(ABC):
     def __init__(self, user_autofill_data):
         self.user_autofill_data = user_autofill_data
-
-    # Helper for easily checking if we can autofill this input category with the user's data
-    def can_autofill_category(self) -> bool:
-        return True
 
     def is_positive_answer(self, text: str) -> bool:
         return any([answer in text.lower() for answer in ["yes", "true"]])
@@ -50,88 +49,133 @@ class BaseCategoryHandler(ABC):
 
         field_type = classified_input.fieldType
         if self.is_text_field(field_type):
-            return self.handle_text_input(classified_input, other_inputs)
+            return self.fill_text_input(classified_input, other_inputs)
 
         if field_type == FieldType.SELECT:
-            return self.handle_select_input(classified_input, other_inputs)
+            return self.fill_select_input(classified_input, other_inputs)
 
         if field_type == FieldType.RADIO:
-            return self.handle_radio_input(classified_input, other_inputs)
+            return self.fill_radio_input(classified_input, other_inputs)
 
         if field_type == FieldType.CHECKBOX:
-            return self.handle_checkbox_input(classified_input, other_inputs)
+            return self.fill_checkbox_input(classified_input, other_inputs)
 
         raise ValueError(f"unsupported field type {field_type}")
 
-    def handle_text_input(
+    # Helper for easily checking if we can autofill this input category with the user's data
+    @abstractmethod
+    def can_autofill_category(self) -> bool:
+        pass
+
+    @abstractmethod
+    def fill_text_input(
         self, classified_input: ClassifiedInput, other_inputs: ClassifiedInputList
     ) -> str | None:
         raise NotImplementedError("Text input handler must be implemented")
 
-    def handle_select_input(
+    def fill_select_input(
         self, classified_input: ClassifiedInput, other_inputs: ClassifiedInputList
     ) -> str | None:
         # If no other behavior is defined, we can just use the text input handler and pray
-        return self.handle_text_input(classified_input, other_inputs)
+        return self.fill_text_input(classified_input, other_inputs)
 
-    def handle_radio_input(
+    @abstractmethod
+    def fill_radio_input(
         self, classified_input: ClassifiedInput, other_inputs: ClassifiedInputList
     ) -> bool | None:
         raise NotImplementedError("No handler created for this radio input")
 
-    def handle_checkbox_input(
+    def fill_checkbox_input(
         self, classified_input: ClassifiedInput, other_inputs: ClassifiedInputList
     ) -> bool | None:
-        return self.handle_radio_input(classified_input, other_inputs)
+        return self.fill_radio_input(classified_input, other_inputs)
 
+    # Saving Methods
     def save_filled_value(
         self, classified_input: ClassifiedInput
     ) -> Union[SaveInstruction, list[SaveInstruction]]:
         field_type = classified_input.fieldType
         if self.is_text_field(field_type) or field_type == FieldType.SELECT:
-            return self.save_text_value(classified_input)
+            return self.save_text_input(classified_input)
 
         elif field_type == FieldType.CHECKBOX or field_type == FieldType.RADIO:
-            return self.save_checkable_value(classified_input)
+            return self.save_checkable_input(classified_input)
 
         raise ValueError(f"Unsupported field type for save {field_type}")
 
-    def save_text_value(
+    @abstractmethod
+    def save_text_input(
         self, classified_input: ClassifiedInput
     ) -> Union[SaveInstruction, list[SaveInstruction]]:
-        raise NotImplementedError(
-            f"No save handler created for text input on {self.__class__.__name__}"
-        )
+        pass
 
-    def save_checkable_value(
+    @abstractmethod
+    def save_checkable_input(
         self, classified_input: ClassifiedInput
     ) -> Union[SaveInstruction, list[SaveInstruction]]:
-        raise NotImplementedError(
-            f"No save handler created for checkable input on {self.__class__.__name__}"
+        pass
+
+
+#     @override
+#     def handle_text_input(
+#         self, classified_input: ClassifiedInput, other_inputs: ClassifiedInputList
+#     ) -> str | None:
+#         return self.get_text()
+
+#     def save_text_value(
+#         self, classified_input: ClassifiedInput
+#     ) -> Union[SaveInstruction, list[SaveInstruction]]:
+#         return SaveInstruction(
+#             value=classified_input.value,
+#             path=self.get_value_path(),
+#         )
+
+
+class TextOnlyCategoryHandler(BaseCategoryHandler, ABC):
+    @override
+    def fill_radio_input(
+        self, classified_input: ClassifiedInput, other_inputs: ClassifiedInputList
+    ) -> bool | None:
+        raise ValueError(
+            f"TextOnlyCategoryHandler {self.__class__.__name__} can only handle text fields, got {classified_input.fieldType} while filling for label {classified_input.label}"
+        )
+
+    @override
+    def save_checkable_input(
+        self, classified_input: ClassifiedInput
+    ) -> SaveInstruction | list[SaveInstruction]:
+        raise ValueError(
+            f"TextOnlyCategoryHandler {self.__class__.__name__} can only handle text fields, got {classified_input.fieldType} while saving for label {classified_input.label}"
         )
 
 
-class TextOnlyCategoryHandler(BaseCategoryHandler):
+class SimpleTextOnlyCategoryHandler(TextOnlyCategoryHandler, ABC):
+    def _get_text(self) -> str:
+        return dictor(self.user_autofill_data, self.get_value_path().replace("/", "."))
+
+    @override
     def can_autofill_category(self) -> bool:
-        return self.get_text() != ""
+        return self._get_text() is not None
 
-    def get_text(self):
-        return ""
-
-    def handle_text_input(
+    @override
+    def fill_text_input(
         self, classified_input: ClassifiedInput, other_inputs: ClassifiedInputList
     ) -> str | None:
-        return self.get_text()
+        return self._get_text()
 
-    def get_autofill_value(
-        self, classified_input: ClassifiedInput, other_inputs: ClassifiedInputList
-    ):
-        if (
-            not self.is_text_field(classified_input.fieldType)
-            or classified_input.fieldType == FieldType.SELECT
-        ):
-            raise ValueError(
-                f"TextOnlyCategoryHandler can only handle text fields, got {classified_input.fieldType} for category {classified_input.category} - label {classified_input.label}"
-            )
+    @override
+    def save_text_input(
+        self, classified_input: ClassifiedInput
+    ) -> SaveInstruction | list[SaveInstruction]:
+        if classified_input.value == "" or classified_input.value is None:
+            return []
 
-        return self.handle_text_input(classified_input, other_inputs)
+        return {
+            "value": classified_input.value,
+            "path": self.get_value_path(),
+        }
+
+    @abstractmethod
+    def get_value_path(self) -> str:
+        # ie "name/first_name"
+        pass
