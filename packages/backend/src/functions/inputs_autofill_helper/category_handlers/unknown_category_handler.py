03bfe9c1f80455e97576d699fc005affb442fb2f
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2025-06-22 03:21:45 -0700

    filled in unknonw category handler

--- packages/backend/src/functions/inputs_autofill_helper/category_handlers/unknown_category_handler.py
+++ packages/backend/src/functions/inputs_autofill_helper/category_handlers/unknown_category_handler.py
@@ -1,6 +1,7 @@
 from functions.inputs_autofill_helper.autofill_schema import (
     ClassifiedInput,
     ClassifiedInputList,
+    SaveInstruction,
 )
 from functions.inputs_autofill_helper.category_handlers.base_category_handler import (
     BaseCategoryHandler,
@@ -12,3 +13,26 @@ class UnknownCategoryHandler(BaseCategoryHandler):
         self, classified_input: ClassifiedInput, other_inputs: ClassifiedInputList
     ):
         return None
+
+    def can_autofill_category(self) -> bool:
+        return False
+
+    def fill_text_input(
+        self, classified_input: ClassifiedInput, other_inputs: ClassifiedInputList
+    ) -> str | None:
+        return None
+
+    def fill_radio_input(
+        self, classified_input: ClassifiedInput, other_inputs: ClassifiedInputList
+    ) -> bool | None:
+        return None
+
+    def save_text_input(
+        self, classified_input: ClassifiedInput
+    ) -> SaveInstruction | list[SaveInstruction]:
+        return []
+
+    def save_checkable_input(
+        self, classified_input: ClassifiedInput
+    ) -> SaveInstruction | list[SaveInstruction]:
+        return []
