9da1945099ec01fc86471f85682d4ee1274542ee
Michael Dickinson <michael.dickinson3949@gmail.com>
2025-09-19 16:35:50 -0700

    update frontend constants to be in one file

--- packages/frontend/firebase.ts
+++ packages/frontend/firebase.ts
@@ -1,15 +1,24 @@
 import { initializeApp } from 'firebase/app'
 import { getStorage } from 'firebase/storage'
 import { browserLocalPersistence, getAuth, GoogleAuthProvider, setPersistence } from 'firebase/auth'
+import {
+  PUBLIC_FIREBASE_API_KEY,
+  PUBLIC_FIREBASE_APP_ID,
+  PUBLIC_FIREBASE_AUTH_DOMAIN,
+  PUBLIC_FIREBASE_MEASUREMENT_ID,
+  PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  PUBLIC_FIREBASE_PROJECT_ID,
+  PUBLIC_FIREBASE_STORAGE_BUCKET,
+} from '@/constants'
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
+  apiKey: PUBLIC_FIREBASE_API_KEY,
+  authDomain: PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: PUBLIC_FIREBASE_APP_ID,
+  measurementId: PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
 export const app = initializeApp(firebaseConfig)
--- packages/frontend/src/hooks/useAuth.ts
+++ packages/frontend/src/hooks/useAuth.ts
@@ -2,6 +2,7 @@ import { onAuthStateChanged, User } from 'firebase/auth'
 import { auth } from '../../firebase'
 import { useState, useEffect } from 'react'
 import Cookies from 'js-cookie'
+import { ENVIRONMENT_NAME } from '@/constants'
 
 type AnonymousUser = {
   uid: string
@@ -14,7 +15,7 @@ const COOKIE_OPTIONS: Cookies.CookieAttributes = {
   // expire after 1 hour (tokens auto-expire in ~1h)
 
   // only send over HTTPS in production -- TALK ABOUT POTENTIAL BACKEND
-  secure: process.env.NODE_ENV === 'production', // TO TEST LOCALLY change this to secure: false
+  secure: ENVIRONMENT_NAME === 'production', // TO TEST LOCALLY change this to secure: false
   // mitigate CSRF
   sameSite: 'Lax',
   // make it available site-wide
